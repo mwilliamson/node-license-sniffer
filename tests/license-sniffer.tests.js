7e58e78165f15f974fc8267b8f2ba00a62e6bf41
Michael Williamson <mike@zwobble.org>
2013-04-13 16:46:04 +0100

    Add support for aliases in licenses.json

--- tests/license-sniffer.tests.js
+++ tests/license-sniffer.tests.js
@@ -29,7 +29,6 @@ describe("license-sniffer.sniff", function() {
     
     it("uses license template if package.json contains license but LICENSE does not exist", function(done) {
         withTemporaryModule("test-module", function(moduleDirPath) {
-            var bsdLicenseText = licenseText("bsd-2-clause");
             fs.writeFileSync(
                 path.join(moduleDirPath, "package.json"),
                 JSON.stringify({name: "test-module", license: "MIT"})
@@ -44,6 +43,24 @@ describe("license-sniffer.sniff", function() {
             }); 
         });
     });
+    
+    it("uses license alias to find license template", function(done) {
+        withTemporaryModule("test-module", function(moduleDirPath) {
+            fs.writeFileSync(
+                path.join(moduleDirPath, "package.json"),
+                JSON.stringify({name: "test-module", license: "BSD"})
+            );
+            licenseSniffer.sniff(moduleDirPath, function(err, license) {
+                assert.ifError(err);
+                var expectedPrefix =
+                    "Copyright (c) 2013, test-module\n" +
+                    "All rights reserved.";
+                assert.equal(license.text.substring(0, expectedPrefix.length), expectedPrefix);
+                done();
+            }); 
+        });
+    });
+    
     
     
     it("errors if package.json doesn't exist", function(done) {
