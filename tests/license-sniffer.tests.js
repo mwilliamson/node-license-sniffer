dbd9b2ca89a8d3150fd2e45a58b75c6acf1e3ee2
Michael Williamson <mike@zwobble.org>
2016-06-21 20:55:35 +0100

    Use more specific license in package.json

--- tests/license-sniffer.tests.js
+++ tests/license-sniffer.tests.js
@@ -16,7 +16,7 @@ describe("license-sniffer.sniff", function() {
     it("detects itself as BSD", function(done) {
         licenseSniffer.sniff(path.join(__dirname, ".."), function(err, license) {
             assert.ifError(err);
-            assert.deepEqual(license.names, ["BSD"]);
+            assert.deepEqual(license.names, ["BSD-2-Clause"]);
             done();
         });
     });
